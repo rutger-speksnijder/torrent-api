e8ab9b59a9171ad5200dfc78e797e5a0f7536a52
Rutger Speksnijder <rutgerspeksnijder@hotmail.com>
2017-01-01 13:36:12 +0100

    Added converting torrents to arrays and /all method

--- TorrentApi.php
+++ TorrentApi.php
@@ -57,7 +57,6 @@ class TorrentApi
 
         // Get request data
         $this->request = $this->getRequestData();
-        var_dump($this->request);
         if (empty($this->request['username']) || empty($this->request['password'])) {
             $this->unauthorized();
             exit;
@@ -82,13 +81,19 @@ class TorrentApi
      */
     private function getRequestData()
     {
+        // Check php://input for PUT/PATCH/DELETE requests
         parse_str(file_get_contents('php://input'), $data);
-        if (empty($data) && !empty($_POST)) {
-            return $_POST;
-        } elseif (empty($data) && empty($_POST)) {
-            return $_GET;
+        if (!empty($data)) {
+            return $data;
         }
-        return $data;
+
+        // Check the POST variable for data
+        if (!empty($_POST)) {
+            return $_POST;
+        }
+
+        // Return the GET variable
+        return $_GET;
     }
 
     /**
@@ -105,9 +110,9 @@ class TorrentApi
 
         // Torrent routes
         $router->get('/all', [$this, 'all']);
+        $router->post('/add', [$this, 'add']);
         $router->get('/get/([0-9]+)', [$this, 'getById']);
         $router->get('/get/([a-zA-Z0-9]+)', [$this, 'getByHash']);
-        $router->post('/add', [$this, 'add']);
         $router->delete('/delete/([0-9]+)', [$this, 'deleteById']);
         $router->delete('/delete/([a-z-A-Z0-9]+)', [$this, 'deleteByHash']);
 
@@ -150,9 +155,12 @@ class TorrentApi
      */
     public function all()
     {
-        $torrents = $this->transmission->all();
-        echo '<pre>';
-        var_dump($torrents);
+        // Get all torrents and convert them to arrays
+        $torrents = $this->convertTorrentsToArrays($this->transmission->all());
+
+        // Output the data
+        $this->result = ['torrents' => $torrents];
+        return $this->output();
     }
 
     /**
@@ -175,5 +183,91 @@ class TorrentApi
 
         // Return the object
         return $this;
+    }
+
+    /**
+     * Converts torrent objects to arrays.
+     *
+     * @param array $torrents The torrent objects.
+     *
+     * @return array An array with torrents converted to arrays.
+     */
+    private function convertTorrentsToArrays($torrents)
+    {
+        // Get the mapping for each object type
+        $fileMapping = Transmission\Model\File::getMapping();
+        $peerMapping = Transmission\Model\Peer::getMapping();
+        $torrentMapping = Transmission\Model\Torrent::getMapping();
+        $trackerMapping = Transmission\Model\Tracker::getMapping();
+        $trackerStatsMapping = Transmission\Model\TrackerStats::getMapping();
+
+        // loop through the torrents
+        $data = [];
+        foreach ($torrents as $torrent) {
+            // Get the torrent data
+            $torrentData = $this->convertObjectToArray($torrent, $torrentMapping);
+
+            // Convert files
+            foreach ($torrentData['files'] as $key => $file) {
+                $torrentData['files'][$key] = $this->convertObjectToArray($file, $fileMapping);
+            }
+
+            // Convert peers
+            foreach ($torrentData['peers']  as $key => $peer) {
+                $torrentData['peers'][$key] = $this->convertObjectToArray($peer, $peerMapping);
+            }
+
+            // Convert trackers
+            foreach ($torrentData['trackers'] as $key => $tracker) {
+                $torrentData['trackers'][$key] = $this->convertObjectToArray($tracker, $trackerMapping);
+            }
+
+            // Convert tracker stats
+            foreach ($torrentData['trackerStats'] as $key => $trackerStats) {
+                $torrentData['trackerStats'][$key] = $this->convertObjectToArray($trackerStats, $trackerStatsMapping);
+            }
+
+            // Add the torrent data to the collection
+            $data[] = $torrentData;
+        }
+
+        // Return the converted torrents
+        return $data;
+    }
+
+    /**
+     * Converts object data to array.
+     *
+     * @param object $object The object to convert.
+     * @param array $mapping The object's mapping array.
+     *
+     * @return array The data array.
+     */
+    private function convertObjectToArray($object, $mapping)
+    {
+        // Check arguments
+        if (!is_object($object) || !is_array($mapping) || empty($mapping)) {
+            throw new \InvalidArgumentException("Invalid arguments for convertObjectToArray.");
+        }
+
+        // Loop through the object's mapping
+        $data = [];
+        foreach ($mapping as $dest => $source) {
+            // Check if the object has a getProperty method
+            $method = 'get' . ucfirst($source);
+            if (method_exists($object, $method)) {
+                $data[$source] = $object->{$method}();
+                continue;
+            }
+
+            // Check if the object has an isProperty method
+            $method = 'is' . ucfirst($source);
+            if (method_exists($object, $method)) {
+                $data[$source] = $object->{$method}();
+            }
+        }
+
+        // Return the data array
+        return $data;
     }
 }
